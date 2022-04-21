73a726d6d8763d797e7e9c65a07fa0a1ffefea9b
VoVanThanh <thanhvo618@gmail.com>
2022-04-21 16:58:39 +0700

    fix deploy

--- api/settings.py
+++ api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import json
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -172,5 +173,4 @@ CORS_ALLOW_CREDENTIALS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import django_on_heroku
 django_on_heroku.settings(locals())
